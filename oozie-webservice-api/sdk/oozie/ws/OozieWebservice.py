bcbd5397df7ed87d37c2fb4d80dc27a1ff4a387d
118k <fluorite118@gmail.com>
2018-09-09 22:36:35 +0900

    우지 웹서비스 생성 클래스 추가

---
+++ oozie-webservice-api/sdk/oozie/ws/OozieWebservice.py
@@ -0,0 +1,21 @@
+'''
+Created on 2018. 9. 9.
+
+@author: whitebeard
+'''
+from sdk.oozie.ws.api import versions
+from sdk.oozie.ws.api import admin
+from sdk.oozie.ws.api import job
+from sdk.oozie.ws.api import jobs
+
+class OozieWebervice(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    
