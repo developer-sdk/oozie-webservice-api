530159c3fb3e3bac0729a1b522863882a3f47986
118k <fluorite118@gmail.com>
2018-09-13 23:09:21 +0900

    add managing job action

--- oozie-webservice-api/sdk/oozie/ws/OozieWebservice.py
+++ oozie-webservice-api/sdk/oozie/ws/OozieWebservice.py
@@ -14,6 +14,8 @@ https://oozie.apache.org/docs/4.2.0/WebServicesAPI.html
 from sdk.oozie.ws import common
 from sdk.oozie.ws.api import versions
 from sdk.oozie.ws.api import admin
+from sdk.oozie.ws.api import jobs
+from sdk.oozie.ws.api import job
 from sdk.oozie.ws.common import check_param_values
 
 class OozieWebservice(object):
@@ -22,6 +24,8 @@ class OozieWebservice(object):
     '''
     
     _OOZIE_SYSTEM_MODE_ = ["NORMAL", "NOWEBSERVICE", "SAFEMODE"]
+    _PROXY_JOB_TYPE_ = ["mapreduce", "pig", "hive", "sqoop"]
+    _MANAGING_JOB_ACTION_ = [ 'start', 'suspend', 'resume', 'kill', 'dryrun', 'rerun', 'change', 'ignore' ]
 
     def __init__(self, oozie_ip, oozie_port):
         '''
@@ -66,3 +70,116 @@ class OozieWebservice(object):
            Keep in mind that timers and counters that the Oozie server hasn't incremented yet will not show up.
         '''
         return admin.metrics()
+    
+    def build_version(self):
+        '''  the Oozie build version. '''
+        return admin.build_version()
+    
+    def available_timezones(self):
+        ''' returns the available time zones. '''
+        return admin.available_timezones()
+    
+    def queue_dump(self):
+        '''  returns the queue dump of the Oozie system. This is an administrator debugging feature. '''
+        return admin.queue_dump()
+    
+    def available_oozie_servers(self):
+        '''  returns the list of available Oozie Servers. This is useful when Oozie is configured for High Availability ; if not, it will simply return the one Oozie Server. '''
+        return admin.available_oozie_servers()
+    
+    def list_sharelib(self, keywords=""):
+        ''' request to get list of available sharelib. If the name of the sharelib is passed as an argument (regex supported) then all corresponding files are also listed. '''
+        return admin.list_sharelib(keywords)
+    
+    def update_sharelib(self):
+        '''
+            This webservice call makes the oozie server(s) to pick up the latest version of sharelib present under oozie.service.WorkflowAppService.system.libpath directory 
+            based on the sharelib directory timestamp or reloads the sharelib metafile if one is configured. The main purpose is to update the sharelib on the oozie server without restarting.
+        '''
+        return admin.update_sharelib()
+    
+    def submit_job(self, xml, start=False):
+        '''
+            request with an XML configuration as payload creates a job.
+
+            The type of job is determined by the presence of one of the following 3 properties:
+
+                oozie.wf.application.path : path to a workflow application directory, creates a workflow job
+                oozie.coord.application.path : path to a coordinator application file, creates a coordinator job
+                oozie.bundle.application.path : path to a bundle application file, creates a bundle job
+                
+            A created job will be in PREP status. If the query string parameter 'action=start' is provided in the POST URL, 
+            the job will be started immediately and its status will be RUNNING .
+        '''
+        return jobs.submit_job(xml, start)
+    
+    @check_param_values(1, _PROXY_JOB_TYPE_)
+    def submit_proxy_job(self, jobtype, xml):
+        '''
+            You can submit a Workflow that contains a single Hive, Pig, MapReduce, Sqoop action without writing a workflow.xml. 
+            Any required Jars or other files must already exist in HDFS.
+        '''
+        return jobs.submit_proxy_job(xml, jobtype)
+    
+    @check_param_values(2, _MANAGING_JOB_ACTION_)    
+    def managing_job(self, job_id, action, xml=""):
+        ''' 
+            request starts, suspends, resumes, kills, update or dryruns a job.
+        '''
+        return job.managing_job(job_id, action, xml)
+    
+    def rerunnig_coord_by_action_id(self, coord_id, scope="", refresh=False, nocleanup=False):
+        '''
+            A coordinator job in RUNNING SUCCEEDED , KILLED or FAILED status can be partially rerun by specifying the coordinator actions to re-execute.
+            A rerun request is done with an HTTP PUT request with a coord-rerun action .
+            The type of the rerun can be date or action .
+            The scope of the rerun depends on the type: * date : a comma-separated list of date ranges. 
+                Each date range element is specified with dates separated by :: * action : a comma-separated list of action ranges. 
+                Each action range is specified with two action numbers separated by -
+            The refresh parameter can be true or false to specify if the user wants to refresh an action's input and output events.
+            The nocleanup parameter can be true or false to specify is the user wants to cleanup output events for the rerun actions.
+        '''
+        return job.rerunning_coordinator(coord_id, "action", scope, "", "", refresh, nocleanup)
+    
+    def rerunnig_coord_by_date(self, coord_id, start_date_time="", end_date_time="", refresh=False, nocleanup=False):
+        '''
+            A coordinator job in RUNNING SUCCEEDED , KILLED or FAILED status can be partially rerun by specifying the coordinator actions to re-execute.
+            A rerun request is done with an HTTP PUT request with a coord-rerun action .
+            The type of the rerun can be date or action .
+            The scope of the rerun depends on the type: * date : a comma-separated list of date ranges. 
+                Each date range element is specified with dates separated by :: * action : a comma-separated list of action ranges. 
+                Each action range is specified with two action numbers separated by -
+            The refresh parameter can be true or false to specify if the user wants to refresh an action's input and output events.
+            The nocleanup parameter can be true or false to specify is the user wants to cleanup output events for the rerun actions.
+        '''
+        return job.rerunning_coordinator(coord_id, "date", "", start_date_time, end_date_time, refresh, nocleanup)
+    
+    def rerunnig_bundle_by_action_name(self, bundle_id, cl_names=[], refresh=False, nocleanup=False):
+        '''
+            A coordinator job in RUNNING SUCCEEDED , KILLED or FAILED status can be partially rerun by specifying the coordinators to re-execute.
+            A rerun request is done with an HTTP PUT request with a bundle-rerun action .
+            A comma separated list of coordinator job names (not IDs) can be specified in the coord-scope parameter.
+            
+            The date-scope parameter is a comma-separated list of date ranges. 
+                Each date range element is specified with dates separated by :: . If empty or not included, Oozie will figure this out for you
+            The refresh parameter can be true or false to specify if the user wants to refresh the coordinator's input and output events.
+            The nocleanup parameter can be true or false to specify is the user wants to cleanup output events for the rerun coordinators.
+        '''
+        return job.rerunning_bundle(bundle_id, ",".join(cl_names), "", "", refresh, nocleanup)
+    
+    def rerunnig_bundle_by_date(self, bundle_id, start_date_time="", end_date_time="", refresh=False, nocleanup=False):
+        return job.rerunning_bundle(bundle_id,"", start_date_time, end_date_time, refresh, nocleanup)
+        
+    def change_coord_info(self, coord_id, concurrency="", endtime="", pausetime=""):
+        '''
+            A coordinator job not in KILLED status can have it's endtime, concurrency, or pausetime changed.
+            A change request is done with an HTTP PUT request with a change action .
+            The value parameter can contain any of the following: * endtime: the end time of the coordinator job. 
+                * concurrency: the concurrency of the coordinator job. * pausetime: the pause time of the coordinator job.
+            Multiple arguments can be passed to the value parameter by separating them with a ';' character.
+            If an already-succeeded job changes its end time, its status will become running.
+        '''
+        return job.change_coord_info(coord_id, concurrency, endtime, pausetime)
+        
+    #def job_info(self, job_info):
+    #    return job.job_info(job_id)
--- oozie-webservice-api/test/oozie_ws.py
+++ oozie-webservice-api/test/oozie_ws.py
@@ -1,47 +1,124 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sdk.oozie.ws.api import versions
-from sdk.oozie.ws.api import admin
-from sdk.oozie.ws.api import job
-from sdk.oozie.ws.api import jobs
+from sdk.oozie.ws.OozieWebservice import OozieWebservice
+
+jobxml = '''<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+<property><name>user.name</name><value>hadoop</value></property>
+<property><name>oozie.wf.application.path</name><value>hdfs:///user/hadoop/MR/CL/</value></property>
+<property><name>nominalDate</name><value>20180901</value></property>
+<property><name>nominalTime</name><value>00</value></property>
+</configuration>
+'''
+
+proxyxml = '''<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+    <property>
+        <name>fs.default.name</name>
+        <value>hdfs://localhost:8020</value>
+    </property>
+    <property>
+        <name>mapred.job.tracker</name>
+        <value>localhost:8032</value>
+    </property>
+    <property>
+        <name>user.name</name>
+        <value>hadoop</value>
+    </property>
+    <property>
+        <name>mapred.job.queue.name</name>
+        <value>q2</value>
+    </property>
+    <property>
+        <name>oozie.hive.script</name>
+        <value>
+            show databases
+        </value>
+    </property>
+    <property>
+        <name>oozie.libpath</name>
+        <value>hdfs://localhost:8020/user/hadoop/share/lib</value>
+    </property>
+    <property>
+        <name>oozie.proxysubmission</name>
+        <value>true</value>
+    </property>
+</configuration>
+'''
+
+rerun_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+<property><name>user.name</name><value>hadoop</value></property>
+    <property>
+        <name>oozie.wf.application.path</name>
+        <value>hdfs://localhost:8020/user/hadoop/MR/CL_MR/kr</value>
+    </property>
+    <property>
+        <name>oozie.wf.rerun.skip.nodes</name>
+        <value>PM_ODW_CL</value>
+    </property>
+</configuration>
+'''
 
 def main():
+    
+    oozie = OozieWebservice("localhost", 11000)
+    
     ## version
-    #versions.oozie_versions()
+    #oozie.versions()
     
     ## admin
-    #admin.status()
+    #oozie.status()
     
-    #admin.change_system_mode("NOWEBSERVICE")
-    #admin.change_system_mode("SAFEMODE")
-    #admin.change_system_mode("NORMAL")
+    #oozie.change_system_mode("NOWEBSERVICE")
+    #oozie.change_system_mode("SAFEMODE")
+    #oozie.change_system_mode("NORMAL")
     
-    #admin.os_env()
-    #admin.java_sys_properties()
-    #admin.metrics() --
-    #admin.build_version()
-    #admin.available_timezones()
-    #admin.available_oozie_servers()
-    #admin.list_sharelib()
-    #admin.list_sharelib("hive*")
-    #admin.update_sharelib()
+    #oozie.os_env()
+    #oozie.java_sys_properties()
+    #oozie.configuration()
+    #oozie.metrics() 
+    #oozie.build_version()
+    #oozie.available_timezones()
+    #oozie.queue_dump()
+    #oozie.available_oozie_servers()
+    #oozie.list_sharelib()
+    #oozie.list_sharelib("hive*")
+    #oozie.update_sharelib()
     
-    #job.job_info("0006300-180305053637798-oozie-bpse-W")
-    #job.job_info("0005928-180305053637798-oozie-bpse-C", filters='status=SKIPPED', length=3, order='desc')
-    #job.job_info("0000344-180305053637798-oozie-bpse-C@1")
-    #job.job_info("0000344-180305053637798-oozie-bpse-C@1", "allruns")
-    #job.job_info("0006300-180305053637798-oozie-bpse-W", "definition")
-    #job.job_info("0006300-180305053637798-oozie-bpse-W", "log")
-    #job.job_info("0006300-180305053637798-oozie-bpse-W", "errorlog") --
-    #job.job_info("0005931-180305053637798-oozie-bpse-C", "auditlog")  --
-    #job.job_log("0006300-180305053637798-oozie-bpse-W", logfilter="limit=3;loglevel=WARN")
-    #job.job_graph("0006300-180305053637798-oozie-bpse-W", file_location="C:/Users/User/Desktop/", showkill="false")
-    #job.job_status("0006300-180305053637798-oozie-bpse-W")
+    #oozie.submit_job(jobxml)
+    #oozie.submit_job(jobxml, True)
+    #oozie.submit_proxy_job('hive', proxyxml)
     
-    jobs.jobs_information()
+    #oozie.managing_job("0006357-SAMPLE-oozie-bpse-W", "kill")
+    #oozie.managing_job("0006351-SAMPLE-oozie-bpse-W", "rerun", rerun_xml)
+    #oozie.rerunnig_coord_by_action_id("0005928-SAMPLE-oozie-bpse-C", "23-24")
+    oozie.rerunnig_coord_by_date("0005928-SAMPLE-oozie-bpse-C", "2018-08-19T21:00Z", "2018-08-19T23:00Z")
+        
+    #oozie.rerunnig_bundle_by_action_name("0006373-180305053637798-oozie-bpse-B", ["CL_1"])
+    #oozie.rerunnig_bundle_by_date("0006373-180305053637798-oozie-bpse-B", "2018-09-12T01:00Z", "2018-09-12T02:00Z")
+    
+    #oozie.change_coord_info("0005931-180305053637798-oozie-bpse-C", pausetime="2030-09-12T02:00Z")
+    
+    #oozie.job_info("0006300-SAMPLE-oozie-bpse-W")
+    #job.job_info("0005928-SAMPLE-oozie-bpse-C", filters='status=SKIPPED', length=3, order='desc')
+    #job.job_info("0000344-SAMPLE-oozie-bpse-C@1")
+    #job.job_info("0000344-SAMPLE-oozie-bpse-C@1", "allruns")
+    #job.job_info("0006300-SAMPLE-oozie-bpse-W", "definition")
+    #job.job_info("0006300-SAMPLE-oozie-bpse-W", "log")
+    #job.job_info("0006300-SAMPLE-oozie-bpse-W", "errorlog") --
+    #job.job_info("0005931-SAMPLE-oozie-bpse-C", "auditlog")  --
+    #job.job_log("0006300-SAMPLE-oozie-bpse-W", logfilter="limit=3;loglevel=WARN")
+    #job.job_graph("0006300-SAMPLE-oozie-bpse-W", file_location="C:/Users/User/Desktop/", showkill="false")
+    #job.job_status("0006300-SAMPLE-oozie-bpse-W")
+    
+    #jobs.jobs_information()
     #jobs.jobs_information(1, 1)
     #jobs.jobs_information(10, 1, status="RUNNING")
-    #jobs.jobs_bulk_modify("kill", 1, 1, jobtype="coordinator", status="RUNNING")
+    #jobs.jobs_bulk_modify("kill", 1, 1, jobtype="wf", status="RUNNING")
+    
+    #job.managing_job("0005931-SAMPLE-oozie-bpse-C", managing_type="action", scope="4")
        
 
 if __name__ == '__main__':
