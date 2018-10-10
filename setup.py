04d0cba8631cd608a302de71fa24706653896525
118k <fluorite118@gmail.com>
2018-10-10 23:01:57 +0900

    add setup files

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'oozie-webservice',
-    version          = '1.0',
+    version          = '0.1',
     description      = 'Python wrapper for Oozie Webservice REST API',
+    long_description = open('README.md').read(),
     author           = 'whitebeard-k',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/oozie-webservice-api',
-    download_url     = 'https://github.com/developer-sdk/oozie-webservice-api/archive/1.0.tar.gz',
-    install_requires = [ ],
+    download_url     = 'https://github.com/developer-sdk/oozie-webservice-api/archive/master.zip',
     packages         = find_packages(exclude = ['test*']),
-    keywords         = ['oozie', 'webservice', 'api'],
-    python_requires  = '>=2.7'
+    keywords         = ['oozie', 'webservice', 'api']
 )
