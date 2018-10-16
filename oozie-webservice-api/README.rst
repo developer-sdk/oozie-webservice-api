4d150e1eef3ef7eb93d6e4b5742dd310bd94f1e6
118k <fluorite118@gmail.com>
2018-10-16 20:38:19 +0900

    modify build file and readme file

---
+++ oozie-webservice-api/README.rst
@@ -0,0 +1,10 @@
+This is Python Library for Oozie Web service api.
+
+This project follows the [Oozie 4.2.0 WebServicesAPI](https://oozie.apache.org/docs/4.2.0/WebServicesAPI.html) document.
+
+Usage
+=====
+
+Install it::
+
+    pip install oozie-webservice-api
--- oozie-webservice-api/setup.py
+++ oozie-webservice-api/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'oozie-webservice-api',
-    version          = '0.2',
+    version          = '0.3',
     description      = 'Python wrapper for Oozie Webservice REST API',
-    long_description = open('README.md').read(),
+    long_description = open('README.rst').read(),
     author           = 'whitebeard-k',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/oozie-webservice-api',
