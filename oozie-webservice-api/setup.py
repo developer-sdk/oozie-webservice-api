f7a440e52b6852b25ffccae9eb1898c5e4117da9
118k <fluorite118@gmail.com>
2018-10-16 20:24:11 +0900

    modify build files

--- oozie-webservice-api/setup.py
+++ oozie-webservice-api/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'oozie-webservice',
-    version          = '0.1',
+    name             = 'oozie-webservice-api',
+    version          = '0.2',
     description      = 'Python wrapper for Oozie Webservice REST API',
     long_description = open('README.md').read(),
     author           = 'whitebeard-k',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/oozie-webservice-api',
     download_url     = 'https://github.com/developer-sdk/oozie-webservice-api/archive/master.zip',
-    packages         = find_packages(exclude = ['test*']),
+    packages         = find_packages(),
     keywords         = ['oozie', 'webservice', 'api']
 )
