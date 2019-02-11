dce7234d2a47ad0dc80d79e48dde714ae73997b9
118k <fluorite118@gmail.com>
2019-02-11 23:41:17 +0900

    HttpBaseApi class init

---
+++ oozie-webservice-api/sdk/HttpRequestApi.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 11, 2019
+
+@author: seo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
