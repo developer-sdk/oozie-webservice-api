f38e4411859f9e3707d3f83bd4096abfebc9ef63
118k <fluorite118@gmail.com>
2019-02-19 22:47:58 +0900

    init

--- oozie-webservice-api/sdk/HttpRequestApi.py
+++ oozie-webservice-api/sdk/HttpRequestApi.py
@@ -1,17 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib, json, urllib2, logging
+import xml.etree.ElementTree as ET
+from urllib2 import HTTPError
 '''
 Created on Feb 11, 2019
 
 @author: seo
 '''
 
-class MyClass(object):
+class HttpRequest(object):
     '''
     classdocs
     '''
-
 
     def __init__(self, params):
         '''
         Constructor
         '''
+        self.logger = logging.getLogger("{0}.{1}".format(self.__class__.qualname__))
         
+    def debug(self, message):
+        self.logger.debug(message)
+    
+    def param_encode(self, params):
+        return urllib.urlencode(params)
+    
+    def request_get(self, request_url):
+        return self.request(request_url, "GET")
+    
+    def request_put(self, request_url, xml=""):
+        return self.request(request_url, "PUT", xml)
+    
+    def request_post(self, request_url, xml):
+        return self.request(request_url, "POST", xml)
+    
+    def request(self, request_url, request_type="GET", xml=""):
+        '''send url and get response'''
+        
+        self.debug("request: {0}".format(request_url))
+        
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request_get = urllib2.Request(request_url, xml, {'Content-Type': 'application/xml'}) if request_type in ("PUT", "POST") else urllib2.Request(request_url) 
+        request_get.get_method = lambda: request_type
+        
+        try:
+            response = opener.open(request_get)
+        except HTTPError as hpe:
+            print("-- HTTPError --")
+            print("  error code: {0}".format(hpe.code))
+            print("     message: {0}".format(hpe.msg))
+            error_html = hpe.read()
+            print("  error html: {0}".format(error_html))
+            return 
+        
+        response_info = response.info()
+        response_body = response.read()
+        
+        self.debug("response header: {0}".format(response_info.getheader("Content-Type")))
+        
+        content_type = response.info().getheader("Content-Type")
+        
+        if content_type is None:
+            return 
+        
+        if content_type.startswith("application/json"): 
+            json_obj = json.loads(response_body)
+            
+            self.debug(json.dumps(json_obj, indent=4, sort_keys=True))
+                
+            return json_obj
+        
+        elif content_type.startswith("application/xml"):
+            root = ET.fromstring(response_body)
+            
+            self.debug("-- response xml --")
+            ET.dump(root)
+            
+            return root
+        elif content_type.startswith("text/plain"):
+            self.debug("-- response txt --")
+            print(response_body)
+                
+            return response_body
+        elif content_type.startswith("image/png"):
+            self.debug("-- response image --")
+            
+            return response_body
+        else:
+            raise ValueError("unknown Content-Type")
