eab503a0a76af1b5a0ebd3d0f76e758c8ec45f22
118k <fluorite118@gmail.com>
2019-06-18 22:07:03 +0900

    modify: add classifiers in setup

--- oozie-webservice-api/setup.py
+++ oozie-webservice-api/setup.py
@@ -2,13 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'oozie-webservice-api',
-    version          = '0.3',
+    version          = '0.5',
     description      = 'Python wrapper for Oozie Webservice REST API',
     long_description = open('README.rst').read(),
-    author           = 'whitebeard-k',
+    author           = 'hs_seo',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/oozie-webservice-api',
     download_url     = 'https://github.com/developer-sdk/oozie-webservice-api/archive/master.zip',
-    packages         = find_packages(),
-    keywords         = ['oozie', 'webservice', 'api']
+    packages         = find_packages(exclude = ['test']),
+    keywords         = ['oozie', 'webservice', 'api'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
