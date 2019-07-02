f42a50b2cb0e8a1fce32bb948449674713df1df8
118k <fluorite118@gmail.com>
2019-07-02 23:26:33 +0900

    modify: python setup information

--- oozie-webservice-api/setup.py
+++ oozie-webservice-api/setup.py
@@ -2,18 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'oozie-webservice-api',
-    version          = '0.5',
+    version          = '1.0.1',
     description      = 'Python wrapper for Oozie Webservice REST API',
     long_description = open('README.rst').read(),
+    license          = 'MIT',
     author           = 'hs_seo',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/oozie-webservice-api',
     download_url     = 'https://github.com/developer-sdk/oozie-webservice-api/archive/master.zip',
     packages         = find_packages(exclude = ['test']),
     keywords         = ['oozie', 'webservice', 'api'],
+    python_requires  = '>=2.6',
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
 )
