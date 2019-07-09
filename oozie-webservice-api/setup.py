8655da7139e7ccc83940fdb9f961ceecc434f596
118k <fluorite118@gmail.com>
2019-07-09 23:11:55 +0900

    modify: version information

--- oozie-webservice-api/setup.py
+++ oozie-webservice-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'oozie-webservice-api',
-    version          = '1.0.1',
+    version          = '1.1.0',
     description      = 'Python wrapper for Oozie Webservice REST API',
     long_description = open('README.rst').read(),
     license          = 'MIT',
